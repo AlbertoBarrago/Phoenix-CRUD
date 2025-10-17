0ced049368feae4a540e6e74cd509a8d20055e37
Alberto Barrago <albertobarrago@gmail.com>
2025-10-17 16:22:22 +0200

    ✨ feat: Add product category and in-stock fields

---
+++ priv/repo/migrations/20251017141800_add_quantity_to_products.exs
@@ -0,0 +1,9 @@
+defmodule SimpleCrud.Repo.Migrations.AddQuantityToProducts do
+  use Ecto.Migration
+
+  def change do
+    alter table(:products) do
+      add :quantity, :integer, default: 0, null: false
+    end
+  end
+end
