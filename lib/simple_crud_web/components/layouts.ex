1f855354ddb2c40338388790d58b5cda381c6d67
Alberto Barrago <albertobarrago@gmail.com>
2025-11-13 21:29:56 +0100

    Format

--- lib/simple_crud_web/components/layouts.ex
+++ lib/simple_crud_web/components/layouts.ex
@@ -35,7 +35,6 @@ defmodule SimpleCrudWeb.Layouts do
 
   def app(assigns) do
     ~H"""
-
     <main class="px-4 py-10 sm:px-6 lg:px-8">
       <div class="mx-auto max-w-2xl space-y-4">
         {render_slot(@inner_block)}
